680526aaa80efe46ad5c713afc64cc56ae8ea666
Valleryikl <vallery.girtie@gmail.com>
2025-09-21 15:09:27 +0200

    build : new Next.js app with TailwindCSS

---
+++ app/src/components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section className="p-10 bg-gradient-to-l to-blue-800 from-black text-white">
+      <h1 className="text-4xl font-bold">The Highest Mountains in the World</h1>
+      <p className="mt-4 text-lg">Explore the most famous peaks on Earth</p>
+    </section>
+  );
+}
