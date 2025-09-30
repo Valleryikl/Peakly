00bd91e1cf48caed7e835c539c843bc793bf5e28
Valleryikl <vallery.girtie@gmail.com>
2025-09-30 17:39:56 +0200

    debag : drop for nav

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -10,36 +10,38 @@ export default function Nav() {
     <nav className="flex items-center justify-between py-[20px] px-[100px] relative ">
       <Link className="text-[40px] z-[11]" href="/">Peakly</Link>
 
-      <ul className="flex gap-[20px] text-[20px] z-[11]">
+      <ul className="flex gap-[10px] text-[20px] z-[11]">
         <li><Link href="/">Home</Link></li>
         <li><Link href="/about">About</Link></li>
 
         <li
-          className="relative"
           onMouseEnter={() => setOpen("mountains")}
-          onMouseLeave={() => setOpen(null)}
           onFocus={() => setOpen("mountains")}
-          onBlur={() => setOpen(null)}
-        >
-          <Link href="/mountains" className="block">Mountains</Link>
-          <ul
-            className={[
-              "absolute left-[-210px] mt-1 w-[1520px] rounded p-2 bg-[#fff] backdrop-blur flex gap-5 justify-center items-center",
-              "transition-all duration-500 ease-out",
-              open === "mountains" ? "opacity-100 translate-y-[-4px] pointer-events-auto" : "opacity-0 translate-y-[-100px] pointer-events-none",
-            ].join(" ")}
           >
-            <ul>
-              <li><Link href="/mountains">Overview</Link></li>
-              <li><Link href="/mountains/search">Search</Link></li>
-              <li><Link href="/mountains/climbers">Climbers</Link></li>
-            </ul>
-            <img className="p-20" src="../favicon.ico" alt="" />
-          </ul>
+          <Link href="/mountains" className="block">Mountains</Link>
+          <div
+            className={[
+              "absolute bg-black transition-all duration-500 ease-out w-screen left-0",
+              "flex gap-[300px] items-center pl-[29%] pt-[20px] pb-[50px]",
+              open === "mountains"
+              ? "translate-y-1 opacity-100 pointer-events-auto"
+              : "-translate-y-6 opacity-0 pointer-events-none",
+            ].join(" ")}
+              onMouseLeave={() => setOpen(null)}
+            >
+              <ul className="space-y-2"
+              >
+                <li><Link href="/mountains">Overview</Link></li>
+                <li><Link href="/mountains/search">Search</Link></li>
+                <li><Link href="/mountains/climbers">Climbers</Link></li>
+              </ul>
+
+              <img className="z-[11]" src="/favicon.ico" alt="" />
+              <div className="black-block"></div>
+          </div>
         </li>
 
         <li
-          className="relative"
           onMouseEnter={() => setOpen("volcanoes")}
           onMouseLeave={() => setOpen(null)}
           onFocus={() => setOpen("volcanoes")}
